629ba78bd0bcc5da78c9b5d4c616b509a9d68068
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-31 20:12:59 +0900

    Add the ethereum section

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -19,16 +19,38 @@ struct ContentView: View {
                         .aspectRatio( contentMode: .fill)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                         .shadow(radius: 5)
-                    VStack(alignment: .leading, spacing: 10) {
+                    VStack(alignment: .leading, spacing: 15) {
                         Text("Equilibrium #3429")
                             .foregroundColor(.white)
                             .font(.largeTitle.bold())
                         Text("Our Equilibrium collection promotes balance and calm.")
                             .foregroundColor(Color(red: 126 / 256, green: 150 / 256, blue: 182 / 256))
+                            .fixedSize(horizontal: false, vertical: true)
                             .font(.subheadline)
+                            .lineSpacing(10)
+                        HStack(alignment: .center) {
+                            HStack(alignment: .center) {
+                                Image("EthereumIcon")
+                                    .resizable()
+                                    .frame(width: 11, height: 18)
+                                Text("0.041ETH")
+                                    .foregroundColor(Color(red: 61 / 256, green: 198 / 256, blue: 203 / 256))
+                                    .font(.subheadline.bold())
+                            }
+                            Spacer()
+                            HStack(alignment: .center) {
+                                Image("ClockIcon")
+                                    .resizable()
+                                    .frame(width: 17, height: 17)
+                                Text("3 days left")
+                                    .foregroundColor(Color(red: 121 / 256, green: 147 / 256, blue: 187 / 256))
+                                    .font(.subheadline.bold())
+                            }
+                        }
                     }
                 }
                 .padding(30)
+                .scaledToFit()
             }
             .clipShape(RoundedRectangle(cornerRadius: 20))
             .padding(.horizontal, 30)
