9f53131af2e591cb0bf27353fb1e674b80237106
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-31 19:14:09 +0900

    Initial Commit

---
+++ Shared/NFTPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTPreviewApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct NFTPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
